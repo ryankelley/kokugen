45b95fd686ffa9dca5659ed3bbe8d0634ee8c826
Nick Lindley <nick@ninecollective.com>
2010-03-09 22:28:49 -0600

    Adding awesome HTTP Method stuff

--- src/kokugen.core/Domain/Company.cs
+++ src/kokugen.core/Domain/Company.cs
@@ -1,8 +1,11 @@
+using Kokugen.Core.Validation;
+
 namespace Kokugen.Core.Domain
 {
     public class Company : Entity
     {
+        [Required]
         public virtual string Name { get; set; }
-        
+        public virtual Address Address { get; set; }
     }
 }
