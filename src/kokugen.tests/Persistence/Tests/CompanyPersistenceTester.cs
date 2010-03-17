0cc791c6a63d75e8227ed1d406dd400b8c722c28
rauhryan <rauh.ryan@gmail.com>
2010-03-16 21:31:23 -0500

    switched unit tests over to sqlite

--- src/kokugen.tests/Persistence/Tests/CompanyPersistenceTester.cs
+++ src/kokugen.tests/Persistence/Tests/CompanyPersistenceTester.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using Kokugen.Core.Domain;
 using FluentNHibernate.Testing;
 using NUnit.Framework;
@@ -10,12 +12,25 @@ namespace Kokugen.Tests.Persistence.Tests
         [Test]
         public void Test_The_Mappings()
         {
-            Specification.CheckProperty(x => x.Name, "Company XYZ")
+            var address = new Address(){City = "Amarillo", State = "TX", StreetLine1 = "8331 N Western", ZipCode = "52136"};
+            Specification
+                .CheckProperty(x => x.Name, "Company XYZ")
+                .CheckProperty(x => x.Address, address, new TypeComparer())
                 .VerifyTheMappings();
-            
+        }
+    }
+
+    public class TypeComparer : IEqualityComparer 
+    {
+        public bool Equals(object x, object y)
+        {
+            return x.GetType() == y.GetType();
         }
 
-        
+        public int GetHashCode(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 
 }
