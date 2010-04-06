913a918639cdb216da87697a265b4c1aa0da4d9e
Ryan Rauh <rauh.ryan@gmail.com>
2010-04-06 13:41:37 -0500

    merge

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("0.1.0.0")]
 [assembly: AssemblyProduct("Kokugen ")]
 [assembly: AssemblyCopyright("Copyright 2010 Nine Collective. All rights reserved.")]
-[assembly: AssemblyTrademark("5792a012e5ca292dfb2f4fd12a6b931983f792ba")]
+[assembly: AssemblyTrademark("0cc791c6a63d75e8227ed1d406dd400b8c722c28")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
