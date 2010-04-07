32387218a8fa44a37447836bf103618fb4c8e9dc
Ryan Rauh <rauh.ryan@gmail.com>
2010-04-07 18:18:56 -0500

    effed up the merge

--- src/kokugen.core/Persistence/Overrides/BoardColumnMappingOverride.cs
+++ src/kokugen.core/Persistence/Overrides/BoardColumnMappingOverride.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 using System;
 using FluentNHibernate.Automapping;
 using FluentNHibernate.Automapping.Alterations;
@@ -14,21 +14,4 @@ namespace Kokugen.Core.Persistence.Overrides
             mapping.Map(x => x.Limit, "[Limit]");
         }
     }
-=======
-using System;
-using FluentNHibernate.Automapping;
-using FluentNHibernate.Automapping.Alterations;
-using FluentNHibernate.Mapping;
-using Kokugen.Core.Domain;
-
-namespace Kokugen.Core.Persistence.Overrides
-{
-    public class BoardColumnMappingOverride : IAutoMappingOverride<BoardColumn>
-    {
-        public void Override(AutoMapping<BoardColumn> mapping)
-        {
-            mapping.Map(x => x.Limit, "[Limit]");
-        }
-    }
->>>>>>> d5bdc39aa5aff7c9f6535ff73797327aed5eb747
 }
