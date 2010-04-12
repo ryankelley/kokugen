854209f30a82b268bcc9f7d8ba5e4e778738d27d
Ryan Rauh <rauh.ryan@gmail.com>
2010-04-12 16:49:51 -0500

    password service is partially implemented

--- src/kokugen.core/Services/Membership/IPasswordHelperService.cs
+++ src/kokugen.core/Services/Membership/IPasswordHelperService.cs
@@ -5,6 +5,7 @@ namespace Kokugen.Core.Services
         string CreatePasswordHash(string password);
         bool ComparePasswordToHash(string password, string passwordHash);
         string RandomPasswordNoHash(int length);
+        string RandomPasswordNoHash(int length, int nonAlphaNumberic);
         string RandomPasswordHashed(int length);
     }
 }
--- src/kokugen.core/Services/Membership/UserService.cs
+++ src/kokugen.core/Services/Membership/UserService.cs
@@ -122,31 +122,4 @@ namespace Kokugen.Core.Services
         }
 
     }
-
-    public class ClearPasswordHelper : IPasswordHelperService
-    {
-        #region Implementation of IPasswordHelperService
-
-        public string CreatePasswordHash(string password)
-        {
-            return password;
-        }
-
-        public bool ComparePasswordToHash(string password, string passwordHash)
-        {
-            return password == passwordHash;
-        }
-
-        public string RandomPasswordNoHash(int length)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string RandomPasswordHashed(int length)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-    }
 }
