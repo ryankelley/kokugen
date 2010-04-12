3d18142caed1eab5192b738c8879f7574d6492ae
Ryan Kelley <rpkelley@gmail.com>
2010-04-12 13:58:24 -0500

    Still working on behavior

--- src/kokugen.web/Behaviors/MustBeAuthorizedBehavior.cs
+++ src/kokugen.web/Behaviors/MustBeAuthorizedBehavior.cs
@@ -1,5 +1,6 @@
 using FubuMVC.Core;
 using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Registration.Nodes;
 using FubuMVC.Core.Runtime;
 using FubuMVC.Core.Security;
 using FubuMVC.Core.Urls;
@@ -7,41 +8,8 @@ using Kokugen.Core.Membership;
 
 namespace Kokugen.Web.Behaviors
 {
-    public class MustBeAuthorizedBehavior : BasicBehavior
+    public class MustBeAuthorizedBehavior
     {
-        private readonly ISecurityContext _securityContext;
-        private readonly IFubuRequest _request;
-        private readonly ISecurityRegistry _securityRegistry;
-        private readonly IActionBehavior _inner;
-
-        public MustBeAuthorizedBehavior( ISecurityContext securityContext, IOutputWriter writer, IUrlRegistry urls, IFubuRequest request, ISecurityRegistry securityRegistry, IActionBehavior inner)
-            : base(PartialBehavior.Ignored)
-        {
-            _securityContext = securityContext;
-            _request = request;
-            _securityRegistry = securityRegistry;
-            _inner = inner;
-        }
-
-        protected override FubuMVC.Core.DoNext performInvoke()
-        {
-            if(_inner.GetType().Name.Contains("ActionInvoker"))
-            {
-                // We must have the action now
-
-                
-            }
-
-
-            if(!_securityContext.IsAuthenticated())
-            {
-                // The User is not logged in, better send back to login form
-            }
-
-            
-
-
-            return DoNext.Continue;
-        }
+        
     }
 }
--- src/kokugen.web/KokugenFubuRegistry.cs
+++ src/kokugen.web/KokugenFubuRegistry.cs
@@ -1,6 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using FubuCore;
 using FubuMVC.Core;
+using FubuMVC.Core.Registration;
+using FubuMVC.Core.Registration.DSL;
+using FubuMVC.Core.Registration.Nodes;
+using FubuMVC.Core.Registration.ObjectGraph;
 using Kokugen.Web.Actions;
 using Kokugen.Web.Actions.Board;
 using Kokugen.Web.Actions.Home;
@@ -50,7 +56,8 @@ namespace Kokugen.Web
                 x.IfIsType<double>(d => d.ToString("N2"));
             });
 
-            //Policies.Add(new AuthorizationBehavior());
+            //Policies.Add<TestBehaviorPolicy>();
+            //Policies.WrapBehaviorChainsWith<MustBeAuthorizedBehavior>();
             //Policies.ConditionallyWrapBehaviorChainsWith<MustBeAuthorizedBehavior>(c => c.OutputType() == typeof (BoardConfigurationModel));
             
             //Policies.WrapBehaviorChainsWith<MustBeAuthorizedBehavior>();
@@ -69,6 +76,17 @@ namespace Kokugen.Web
         }
     }
 
+    public class TestBehaviorPolicy : IConfigurationAction
+    {
+        public void Configure(BehaviorGraph graph)
+        {
+            var myBehavs = graph.Behaviors.Where(c => c.FirstCall().Category == BehaviorCategory.Call).ToList();
+            var myActions = graph.Actions().ToList();
+            myActions.Each(act => act.AddBefore(Wrapper.For<MustBeAuthorizedBehavior>()));
+            //graph.Behaviors.Where(c => c.FirstCall().Category == BehaviorCategory.Call).Each(c => c.Prepend(new Wrapper(typeof(MustBeAuthorizedBehavior)))); 
+        }
+    }
+
     //public class KokugenViewAttachmentStrategy : IViewsForActionFilter
     //{
     //    public IEnumerable<IViewToken> Apply(ActionCall call, ViewBag views)
@@ -81,5 +99,47 @@ namespace Kokugen.Web
     //                .Select(view => view);
     //    }
     //}
+    public static class PolicyExtensions
+    {
+        public static PoliciesExpression WrapWithData<T>(this PoliciesExpression expr, object dataToPass)
+        {
+            var configAction = new VisitBehaviorsAction(v =>
+                v.Actions += chain => chain.Prepend(new AuthWrapper()),
+                "wrap with the {0} behavior".ToFormat(typeof(T).Name));
 
+            
+            expr.Add(configAction);
+
+
+            return expr;
+        }
+    }
+
+    public class AuthWrapper : BehaviorNode
+    {
+        private ObjectDef _objectDef;
+
+        public AuthWrapper()
+        {
+            _objectDef = new ObjectDef
+            {
+                Type = typeof(MustBeAuthorizedBehavior)
+            };
+        }
+
+        protected override ObjectDef buildObjectDef()
+        {
+            return _objectDef;
+        }
+
+        public override BehaviorCategory Category
+        {
+            get { return BehaviorCategory.Wrapper; }
+        }
+
+        public override string ToString()
+        {
+            return "Wrapped by " + _objectDef.Type.FullName;
+        }
+    }
 }
