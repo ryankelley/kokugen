b0d3afa7b8838a65d3edfb2974dea187020a1a71
Ryan Kelley <rpkelley@gmail.com>
2010-04-14 16:34:15 -0500

    working on master page cleanup

---
+++ src/kokugen.web/Shared/Super.Master.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Web.UI;
+using FubuMVC.Core.View;
+using Kokugen.Web.Actions;
+
+namespace Kokugen.Web.Shared
+{
+    public class Super : FubuMasterPage
+    {
+        
+    }
+}
