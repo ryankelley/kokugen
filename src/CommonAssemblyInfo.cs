245986734003952a2ba87a3bbc0cea57ae17887e
Ryan Rauh <rauh.ryan@gmail.com>
2010-04-14 08:20:46 -0500

    updated fubu to trunk latest

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("0.1.0.0")]
 [assembly: AssemblyProduct("Kokugen ")]
 [assembly: AssemblyCopyright("Copyright 2010 Nine Collective. All rights reserved.")]
-[assembly: AssemblyTrademark("3c05b75ac1fa29449579f461b1eb22508f132b5d")]
+[assembly: AssemblyTrademark("956d2ffd4d38cec1d148571038ead5f7fba4619f")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
