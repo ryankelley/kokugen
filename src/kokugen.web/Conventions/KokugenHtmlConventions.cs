67349525c4e01fb407af896de46ee9e92ca45538
Ryan Kelley <rpkelley@gmail.com>
2010-04-15 13:41:29 -0500

    working on markdown support

--- src/kokugen.web/Conventions/KokugenHtmlConventions.cs
+++ src/kokugen.web/Conventions/KokugenHtmlConventions.cs
@@ -1,8 +1,6 @@
 using System;
 using FubuCore;
-using FubuCore.Reflection;
 using FubuMVC.UI;
-using FubuMVC.UI.Configuration;
 using FubuMVC.UI.Tags;
 using HtmlTags;
 using Kokugen.Core;
@@ -102,36 +100,6 @@ namespace Kokugen.Web.Conventions
         private static bool IsFloatingPoint(Type type)
         {
             return type == typeof(decimal) || type == typeof(float) || type == typeof(double);
-        }
-    }
-
-    public class EditInPlaceBuilder : ElementBuilder
-    {
-        protected override bool matches(AccessorDef def)
-        {
-            return true;
-        }
-
-        public override HtmlTag Build(ElementRequest request)
-        {
-            var tag = new HtmlTag("div").Text(request.StringValue()).AddClass("editable").Id(request.Accessor.Name);
-
-            var options = new EditOptions();
-
-            options.MultiLine = request.Accessor.Name == "Details";
-            options.RequiresExplicitUserActionForSave = true;
-            
-            options.MaximumLength = request.Accessor.PropertyType.Equals(typeof(string)) ? Entity.UnboundedStringLength : 0;
-            options.IsDate = request.Accessor.PropertyType.IsDateTime();
-            options.IsTime = request.Accessor.Name.ToLower().Contains("time");
-            options.IsNumber = request.Accessor.PropertyType.IsIntegerBased() || request.Accessor.PropertyType.IsFloatingPoint();
-            options.Required = request.Accessor.HasAttribute<RequiredAttribute>();
-            options.PlaceholderText = "Double-Click to edit " + request.Accessor.Name.ToLower() + ".";
-
-            var data = options.ToJson();    
-
-            tag.Attr("data", "{editoptions:"+data+"}");
-            return tag;
         }
     }
 
