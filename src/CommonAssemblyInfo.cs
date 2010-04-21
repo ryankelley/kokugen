88aa7044b218b991dfc85ee7d0c3885cb8365c76
Ryan Rauh <rauh.ryan@gmail.com>
2010-04-21 16:47:28 -0500

    rake changed the assembly file

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("0.1.0.0")]
 [assembly: AssemblyProduct("Kokugen ")]
 [assembly: AssemblyCopyright("Copyright 2010 Nine Collective. All rights reserved.")]
-[assembly: AssemblyTrademark("d295a3b464a1a12fe45b04505cc26d7d50b6e4f1")]
+[assembly: AssemblyTrademark("37ea2326ba17e0697f9c3b9ad54d8130771156cb")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
 
