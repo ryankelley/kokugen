a74f8af96d99eb98e92f987433036303c32fa17e
Caleb Briggs <caleb@ninecollective.com>
2010-05-04 16:45:48 -0500

    added script for super user and altering roles

---
+++ dbChangeScripts/update/0029_Added_SuperUser_and_altered_Roles.sql
@@ -0,0 +1,84 @@
+﻿/*
+Run this script on:
+
+        localhost.KokugenDataVersioned    -  This database will be modified
+
+to synchronize it with:
+
+        localhost.KokugenData
+
+You are recommended to back up your database before running this script
+
+Script created by SQL Compare version 8.1.0 from Red Gate Software Ltd at 5/4/2010 4:40:43 PM
+
+*/
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Altering [dbo].[Roles]'
+GO
+ALTER TABLE [dbo].[Roles] ADD
+[Project_id] [uniqueidentifier] NULL
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating [dbo].[SuperUser]'
+GO
+CREATE TABLE [dbo].[SuperUser]
+(
+[Id] [uniqueidentifier] NOT NULL
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Creating primary key [PK__SuperUse__3214EC0737A5467C] on [dbo].[SuperUser]'
+GO
+ALTER TABLE [dbo].[SuperUser] ADD CONSTRAINT [PK__SuperUse__3214EC0737A5467C] PRIMARY KEY CLUSTERED  ([Id])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Adding foreign keys to [dbo].[Roles]'
+GO
+ALTER TABLE [dbo].[Roles] ADD
+CONSTRAINT [fk_Role_to_Project] FOREIGN KEY ([Project_id]) REFERENCES [dbo].[Projects] ([Id])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+PRINT N'Adding foreign keys to [dbo].[SuperUser]'
+GO
+ALTER TABLE [dbo].[SuperUser] ADD
+CONSTRAINT [fk_SuperUser] FOREIGN KEY ([Id]) REFERENCES [dbo].[Users] ([Id])
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The database update succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The database update failed'
+GO
+DROP TABLE #tmpErrors
+GO
